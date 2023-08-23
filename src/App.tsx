68a7ce79f2a2c56591f23cba86006d5b5fba444f
Sunmin Koak <smkoak@gmail.com>
2023-08-23 22:49:30 +0900

    docs: pr템플릿 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 
 const App = () => {
   return (
-    <div className="bg-gray-50 p-8 mx-2 rounded-2xl text-center text-lg md:text-xl" />
+    <div className="bg-gray-50 p-8 mx-2 rounded-2xl text-center text-lg sm:text-xl" />
   )
 }
 
