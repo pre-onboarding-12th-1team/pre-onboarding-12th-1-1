7fa4e50d4627eed925f35b1f89729bde96e59c97
sujikim <sujikim@student.42seoul.kr>
2023-08-24 23:46:24 +0900

    feat: 로그인 네트워크 요청 구현
    - 성공시 토큰 저장 및 /todo 경로로 리다이렉트
    
    Co-authored-by: Jieun Lee <jieeeun2@users.noreply.github.com>

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -1,6 +1,9 @@
+import Auth from 'apis/auth';
 import AuthButton from 'components/common/AuthButton';
 import InputWithErrorMessage from 'components/common/InputWithErrorMessage';
-import { useState } from 'react';
+import { FormEventHandler, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { setToken } from 'utils/localStorage';
 import { emailPolicy, passwordPolicy } from 'utils/validation';
 
 const SignInPage = () => {
@@ -9,14 +12,25 @@ const SignInPage = () => {
   const { isValidated: isEmailValidated, errorMessage: emailError } = emailPolicy(email);
   const { isValidated: isPasswordValidated, errorMessage: passwordError } =
     passwordPolicy(password);
+  const navigate = useNavigate();
+
+  const handleSubmit: FormEventHandler = async (e) => {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    await Auth.signin(email, password).then((response) => {
+      if (response && response.status === 200) {
+        setToken(response.data.access_token);
+        navigate('/todo');
+      }
+    });
+  };
 
   return (
     <main className="h-screen flex flex-col justify-center items-center">
       <form
         className="flex flex-col justify-center items-center gap-2 w-72"
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
+        onSubmit={handleSubmit}
       >
         <InputWithErrorMessage
           data-testid="email-input"
