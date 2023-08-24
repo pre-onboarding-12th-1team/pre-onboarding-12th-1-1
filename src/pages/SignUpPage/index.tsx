b619199298ac65af7cd203ec470b247df0d7e245
sujikim <sujikim@student.42seoul.kr>
2023-08-24 23:52:06 +0900

    feat: 회원가입 네트워크 요청 구현
    - 성공시 /signin 경로로 리다이렉트
    
    Co-authored-by: Jieun Lee <jieeeun2@users.noreply.github.com>

--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -1,6 +1,8 @@
+import Auth from 'apis/auth';
 import AuthButton from 'components/common/AuthButton';
 import InputWithErrorMessage from 'components/common/InputWithErrorMessage';
-import { useState } from 'react';
+import { FormEventHandler, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { emailPolicy, passwordPolicy } from 'utils/validation';
 
 const SignUpPage = () => {
@@ -9,14 +11,24 @@ const SignUpPage = () => {
   const { isValidated: isEmailValidated, errorMessage: emailError } = emailPolicy(email);
   const { isValidated: isPasswordValidated, errorMessage: passwordError } =
     passwordPolicy(password);
+  const navigate = useNavigate();
+
+  const handleSubmit: FormEventHandler = async (e) => {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    await Auth.signup(email, password).then((response) => {
+      if (response && response.status === 201) {
+        navigate('/signin');
+      }
+    });
+  };
 
   return (
     <main className="h-screen flex flex-col justify-center items-center">
       <form
         className="flex flex-col justify-center items-center gap-2 w-72"
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
+        onSubmit={handleSubmit}
       >
         <InputWithErrorMessage
           data-testid="email-input"
