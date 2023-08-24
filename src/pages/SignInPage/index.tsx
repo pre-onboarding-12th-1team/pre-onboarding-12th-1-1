de5e918be337d1227ae952aea070591384d5bd08
HHB <bcl0206@gmail.com>
2023-08-24 20:20:38 +0900

    feat: 로그인 컴포넌트  초기 ui 및 로직 구현

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -1,3 +1,32 @@
-const SignInPage = () => <div />;
+import { useState } from 'react';
+
+const SignInPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <form
+        className="flex flex-col justify-center items-center gap-2 w-72"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          className="shadow-md outline-none rounded-md w-full h-10 p-3"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="shadow-md outline-none rounded-md w-full h-10 p-3"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="w-full h-10 bg-black  text-white rounded-md">로그인</button>
+      </form>
+    </div>
+  );
+};
 
 export default SignInPage;
